41a287eed27584e40a8e10bffeff01d4565a9b06
Kanishka Wijesekara <kanishka.madhuranga@aircanada.ac>
2023-06-25 18:35:53 +0530

    Updated code

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def model_loading():
 
     return load_model('model/')
 
+def get_label_name_by_id(id_str):
+    id_class_name_dict = {'Late_blight': 2, 'healthy': 9, 'Early_blight': 1, 'Septoria_leaf_spot': 4, 'Tomato_Yellow_Leaf_Curl_Virus': 7, 'Bacterial_spot': 0, 'Target_Spot': 6, 'Tomato_mosaic_virus': 8, 'Leaf_Mold': 3, 'Spider_mites Two-spotted_spider_mite': 5}
+    dict_id = { v:k for (k, v) in id_class_name_dict.items()}
+    return dict_id[id_str]
+
 
 ml_models = {}
 
@@ -35,5 +40,5 @@ async def predict(file: bytes = File(...)):
         array=array/255
         image = np.expand_dims(array, axis = 0)
         tomato_class = np.argmax(ml_models['tomato_model'].predict(image))
-        
-    return tomato_class
+        print(tomato_class)
+    return { "disease_name": get_label_name_by_id(tomato_class.item())}
